6010a6753b5e30f51a3eba4355cf156210f3b4d7
AndersonBones <abones19@hotmail.com>
2026-01-15 20:47:22 -0400

    add internacionalização ExtratoScreen

--- app/src/main/java/br/edu/utfpr/financeflow/ui/extrato/ExtratoHeader.kt
+++ app/src/main/java/br/edu/utfpr/financeflow/ui/extrato/ExtratoHeader.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.edu.utfpr.financeflow.R
 import br.edu.utfpr.financeflow.model.TipoLancamento
 import br.edu.utfpr.financeflow.ui.theme.FinanceFlowTheme
 
@@ -25,7 +27,7 @@ fun ExtratoHeader(
         modifier = Modifier.padding(16.dp)
     ) {
         Text(
-            text = "Saldo Atual",
+            text = stringResource(R.string.current_balance_label_extrato_screen),
             color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f)
         )
 
